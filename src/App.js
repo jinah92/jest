1f15f851b09a5974a47241eaafd0f808004e266b
jinah92 <jinah0818@gmail.com>
2022-02-16 22:56:08 +0900

    jest test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Butotn from "./components/Button";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Butotn />
       </header>
     </div>
   );
